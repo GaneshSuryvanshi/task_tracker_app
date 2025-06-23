7b5e7d8fca19d26460ae30c8a2519c383e6ffb1b
GaneshSuryvanshi <53352526+GaneshSuryvanshi@users.noreply.github.com>
2025-06-24 00:45:31 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,15 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
- apiKey: "AIzaSyCKfkeH3FSWPMztu518Np1JiMcf7T7Rvwk",
-  authDomain: "fsd-task-tracker-app.firebaseapp.com",
-  projectId: "fsd-task-tracker-app",
-  storageBucket: "fsd-task-tracker-app.firebasestorage.app",
-  messagingSenderId: "218670803195",
-  appId: "1:218670803195:web:f263c83a7cf3fb790de85e",
-  measurementId: "G-YQ1HLDQFZY"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
