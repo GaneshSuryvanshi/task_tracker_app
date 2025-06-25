8efccc6655bf240fc69ab3f42b26c9c6da187176
Ganesh.Suryvanhi.Contractor@pepsico.com <ganesh.suryvansh@tigeranalytics.local>
2025-06-25 13:58:06 +0530

    updated starter file

--- start.py
+++ start.py
@@ -5,11 +5,11 @@ import uvicorn
 from backend.main import app  # Import the FastAPI app from main.py
 
 # Mount React static files
-app.mount("/static", StaticFiles(directory="frontend_build/static"), name="static")
+#app.mount("/static", StaticFiles(directory="frontend_build/static"), name="static")
 
-@app.get("/")
-def serve_react_index():
-    return FileResponse("frontend_build/index.html")
+#@app.get("/")
+#def serve_react_index():
+#    return FileResponse("frontend_build/index.html")
 
 if __name__ == "__main__":
     uvicorn.run("start:app", host="0.0.0.0", port=8000)
